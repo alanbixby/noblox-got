34e4b5f694e6199a214495e0823c3c1274d6906b
Alan Bixby <alanrbixby@gmail.com>
2022-10-04 02:06:06 -0400

    feat: preliminary functional shortpolling

---
+++ src/index.d.ts
@@ -0,0 +1,58 @@
+export interface TransactionsResponse {
+  previousPageCursor?: string
+  nextPageCursor?: string
+  data: Transaction[]
+}
+
+export interface Transaction {
+  id: number
+  created: string
+  isPending: boolean
+  agent: Agent
+  details: Details
+  currency: Currency
+}
+
+export interface Agent {
+  id: number
+  type: AgentType
+  name: string
+}
+
+export enum AgentType {
+  User = 'User',
+  Group = 'Group',
+}
+
+export interface Currency {
+  amount: number
+  type: CurrencyType
+}
+
+export enum CurrencyType {
+  Robux = 'Robux',
+}
+
+export interface Details {
+  id?: number
+  name: string
+  type: DetailsType
+  place?: Place
+  payments?: Payment[]
+}
+export interface Payment {
+  created: string
+  currency: Currency
+}
+
+export interface Place {
+  placeId: number
+  universeId: number
+  name: string
+}
+
+export enum DetailsType {
+  Asset = 'Asset',
+  GamePass = 'GamePass',
+  PrivateServer = 'PrivateServer',
+}
